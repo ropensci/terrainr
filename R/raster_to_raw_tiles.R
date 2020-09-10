aa0428172c11488d43a34dd304bd49ca92467ea6
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-09-10 09:40:26 -0400

    Fix raster_to_raw and document

--- R/raster_to_raw_tiles.R
+++ R/raster_to_raw_tiles.R
@@ -40,32 +40,40 @@ raster_to_raw_tiles <- function(input_file, output_prefix, side_length = 4097) {
 
   counter <- 1
 
-  for (i in seq(0, input_raster@nrows, side_length)) {
-    for (j in seq(0, input_raster@ncols, side_length)) {
+  for (i in seq(0, input_raster@nrows, 4097)) {
+    for (j in seq(0, input_raster@ncols, 4097)) {
       gdalUtils::gdal_translate(input_file, temptiffs[[counter]],
-                                srcwin = paste0(i, ", ", j, ", side_length, side_length")
+                                srcwin = paste0(i, ", ", j, ", 4097, 4097")
       )
       names(temptiffs)[[counter]] <-  paste0(output_prefix, "_", i, "_", j, ".raw")
       counter <- counter + 1
     }
   }
 
-  temppngs <- tempfile(fileext = ".png")
+  temppngs <- NULL
+  while (length(temppngs) != length(temptiffs)) {
+    temppngs <- unique(vapply(seq_along(temptiffs),
+                              function(x) tempfile(fileext = ".png"),
+                              character(1)))
+  }
+  names(temppngs) <- names(temptiffs)
+
   for (i in seq_along(temptiffs)) {
     gdalUtilities::gdal_translate(src_dataset = temptiffs[[i]],
-                                  dst_dataset = temppngs,
+                                  dst_dataset = temppngs[[i]],
                                   ot = "UInt16",
                                   strict = FALSE,
                                   scale = c(0, max_raster, 0, (2^16) - 1),
                                   of = "png")
-    processing_image <- magick::image_read(temppngs)
+  }
+
+  for (i in seq_along(temppngs)) {
+    processing_image <- magick::image_read(temppngs[[i]])
     processing_image <- magick::image_convert(processing_image,
                                               format = "RGB",
                                               depth = 16,
                                               interlace = "Plane")
-    magick::image_write(processing_image, names(temptiffs)[[i]])
+    magick::image_write(processing_image, names(temppngs)[[i]])
   }
-
-  return(NULL)
 
 }
