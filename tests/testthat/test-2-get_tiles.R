92f4507cdeb9ea352370e5d4f257fe5d5015ad16
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-10-12 14:56:47 -0400

    Travis testing

--- tests/testthat/test-2-get_tiles.R
+++ tests/testthat/test-2-get_tiles.R
@@ -45,18 +45,9 @@ test_that("get_tiles gets the same tiles twice -- with georeferencing!", {
   expect_equal(length(output_tif), 3)
   expect_equal(length(output_tif[[1]]), 1)
 
-  stored_raster <- raster::raster("testdata/3DEP_gr.tif")
-  test_raster <- raster::raster(output_tif[[1]])
 
-  expect_equal(stored_raster@crs, test_raster@crs)
-  expect_equal(stored_raster@extent, test_raster@extent)
-  expect_equal(
-    raster::cellStats(stored_raster, "max"),
-    raster::cellStats(test_raster, "max")
-  )
-
-  stored_raster <- raster::raster("testdata/NAIPPlus_gr.tif")
-  test_raster <- raster::raster(output_tif[[2]])
+  stored_raster <- raster::raster("testdata/nhd_gr.tif")
+  test_raster <- raster::raster(output_tif[[3]])
 
   expect_equal(stored_raster@crs, test_raster@crs)
   expect_equal(stored_raster@extent, test_raster@extent)
@@ -65,6 +56,7 @@ test_that("get_tiles gets the same tiles twice -- with georeferencing!", {
     raster::cellStats(test_raster, "max")
   )
 })
+
 
 test_that("get_tiles fails as expected", {
   expect_error(get_tiles(list(
