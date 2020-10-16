2dbd104beebc1680c215bc67db22f4e964cd8dc9
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-10-16 11:57:54 -0400

    Edits for magick 2.5.0 on CRAN

--- tests/testthat/test-1-raster_to_raw_tiles.R
+++ tests/testthat/test-1-raster_to_raw_tiles.R
@@ -1,20 +1,18 @@
 test_that("raster_to_raw runs without errors", {
-  # These files aren't included in the repo or package, so can't run these tests
-  # But important to validate they pass locally!
-  skip("raster_to_raw tests must be run manually")
 
   outputs <- raster_to_raw_tiles(
-    input_file = "testdata/test_ortho.tif",
+    input_file = "testdata/merge_rasters_test.tif",
     output_prefix = tempfile(),
     side_length = 4097,
     raw = FALSE
   )
 
-  expect_equal(png::readPNG(outputs[[1]]), "testdata/test_final_1_1.png")
+  expect_equal(png::readPNG(outputs[[1]]),
+               png::readPNG("testdata/raster_to_raw_1.png"))
 
   expect_error(
     raster_to_raw_tiles(
-      input_file = "testdata/test_merge.tif",
+      input_file = "testdata/merge_rasters_test.tif",
       output_prefix = tempfile(),
       side_length = 4097,
       raw = TRUE
